7c4ff15911cf46485a65ad60341bbb5b82784ac5
Shajan Jacob <shajanjp@gmail.com>
2019-10-09 14:48:50 +0530

    disables nosleep and controls

--- js/script.js
+++ js/script.js
@@ -2,13 +2,13 @@ var container = document.getElementsByTagName("body")[0];
 var exitButton = document.getElementsByClassName("exit")[0];
 console.log('no sleep started');
 
-var noSleep = new NoSleep();
-noSleep.enable(); // keep the screen on!
+// var noSleep = new NoSleep();
+// noSleep.enable(); // keep the screen on!
 
-exitButton.addEventListener('click', () => {
-  document.exitFullscreen();
-})
+// exitButton.addEventListener('click', () => {
+//   document.exitFullscreen();
+// })
 
-container.addEventListener("click", function() {
-  document.body.requestFullscreen();
-}, false);
+// container.addEventListener("click", function() {
+//   document.body.requestFullscreen();
+// }, false);
